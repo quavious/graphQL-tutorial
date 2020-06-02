f51710ce759ed7a66f4b7385a2331d0bcbc1086c
quavious <leenowon97@gmail.com>
2020-06-02 23:48:04 +0900

    movieql

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,17 @@
-export let movies = [
-    {
-        id: 0,
-        name: "Star Wars",
-        score: 22
-    },
-      {
-        id: 1,
-        name: "Avengers",
-        score: 8
-    },
-      {
-        id: 2,
-        name: "The Godfather",
-        score: 99
-    },
-      {
-        id: 3,
-        name: "Logan",
-        score: 2
+import fetch from "node-fetch"
+
+let apiUrl = "https://yts.am/api/v2/list_movies.json?"
+
+export const getMovies = (limit, rating) => {
+    let requestUrl = apiUrl;
+    if(limit>0){
+        requestUrl += `limit=${limit}`
     }
-];
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => id === movie.id)
-    return filteredMovies[0]
-};
-
-export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id)
-    if(movies.length > cleanedMovies.length) {
-        movies = cleanedMovies
-        return true
-    } else {
-        return false
+    if(rating>0){
+        requestUrl += `&minimum_rating=${rating}`
     }
+    return fetch(requestUrl)
+    .then(res => res.json())
+    .then(json => json.data.movies)
 }
 
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: movies.length+1,
-        name,
-        score
-    }
-    movies.push(newMovie)
-    return newMovie
-}
