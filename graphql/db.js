265f8ee79ab53441470d44358e5dcec4e86e5de5
quavious <leenowon97@gmail.com>
2020-06-02 23:13:20 +0900

    movies

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,49 @@
-export const people = [
+export let movies = [
     {
         id: 0,
-        name: "nicolas",
-        age: 18,
-        gender: "male"
+        name: "Star Wars",
+        score: 22
     },
-    {
+      {
         id: 1,
-        name: "Jisu",
-        age: 18,
-        gender: "female"
+        name: "Avengers",
+        score: 8
     },
       {
         id: 2,
-        name: "Japan Guy",
-        age: 18,
-        gender: "male"
+        name: "The Godfather",
+        score: 99
     },
       {
         id: 3,
-        name: "Daal",
-        age: 18,
-        gender: "male"
-    },
-      {
-        id: 4,
-        name: "JD",
-        age: 18,
-        gender: "male"
-    },
-      {
-        id: 5,
-        name: "moondaddi",
-        age: 18,
-        gender: "male"
-    },
-      {
-        id: 6,
-        name: "flynn",
-        age: 18,
-        gender: "male"
+        name: "Logan",
+        score: 2
     }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => id === person.id)
-    return filteredPeople[0]
+    const filteredMovies = movies.filter(movie => id === movie.id)
+    return filteredMovies[0]
+};
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id)
+    if(movies.length > cleanedMovies.length) {
+        movies = cleanedMovies
+        return true
+    } else {
+        return false
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: movies.length+1,
+        name,
+        score
+    }
+    movies.push(newMovie)
+    return newMovie
 }
